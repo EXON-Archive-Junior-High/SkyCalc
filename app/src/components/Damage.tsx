72b79c172a9d1b83a18c5bde37e997892467622a
시혁 박 <exon.indiv@gmail.com>
2021-02-06 19:01:25 +0900

    Damage

--- app/src/components/Damage.tsx
+++ app/src/components/Damage.tsx
@@ -2,22 +2,21 @@ import React, { Component } from 'react'
 import Bar from './Bar'
 
 export default class Damage extends Component {
-    public state = { damage: '', strength: '' }
+    public state = { damage: '', strength: '', value: '' }
 
     public handleChangeFirst(e: React.ChangeEvent<HTMLInputElement>) {
         this.setState({ damage: e.target.value })
-        this.change()
     }
 
     public handleChangeSecond(e: React.ChangeEvent<HTMLInputElement>) {
         this.setState({ strength: e.target.value })
-        this.change()
     }
 
     public change() {
         const damage = +this.state.damage
         const strength = +this.state.strength
         console.log(damage + strength)
+        
     }
 
     render() {
@@ -25,22 +24,29 @@ export default class Damage extends Component {
             <div>
                 <Bar />
                 <div className="main">
-                    <div className="float">
-                        <input id="damage" placeholder="100" value={this.state.damage} onChange={this.handleChangeFirst.bind(this)}></input>
-                        <hr/>
-                        <p>Damage</p>
+                    <div className="calc">
+                        <div className="float">
+                            <input id="damage" placeholder="100" value={this.state.damage} onChange={this.handleChangeFirst.bind(this)}></input>
+                            <hr/>
+                            <p>Damage</p>
+                        </div>
+                        <h1 className="float">+</h1>
+                        <div className="float">
+                            <input id="strength" placeholder="100" value={this.state.strength} onChange={this.handleChangeSecond.bind(this)}></input>
+                            <hr/>
+                            <p>Strength</p>
+                        </div>
+                        <h1 className="float" role="button" onClick={this.change.bind(this)}>=</h1>
+                        <div>
+                            <input placeholder="100" value={this.state.value} disabled></input>
+                            <hr/>
+                            <p>Real Damage</p>
+                        </div>
                     </div>
-                    <h1 className="float">+</h1>
-                    <div className="float">
-                        <input id="strength" placeholder="100" value={this.state.strength} onChange={this.handleChangeSecond.bind(this)}></input>
-                        <hr/>
-                        <p>Strength</p>
-                    </div>
-                    <h1 className="float">=</h1>
-                    <div className="float">
-                        <input placeholder="100" disabled></input>
-                        <hr/>
-                        <p>Real Damage</p>
+                    <br />
+                    <div className="formula">
+                        <h1>Formula</h1>
+                        <p>1 + 1 은 귀여미</p>
                     </div>
                 </div>
                 
